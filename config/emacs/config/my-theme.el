afd81bd7ce1eab0362a2284620fcd032df13848e
Michael Lin <mtlin@google.com>
2014-05-10 15:36:33 -0400

    new

--- config/emacs/config/my-theme.el
+++ config/emacs/config/my-theme.el
@@ -3,11 +3,12 @@
          (gray "brightcyan")
          (cyan "cyan")
          (red "red")
-         (green "yellow")
+         (orange "yellow")
          (blue "blue")
          (lightgray "white")
          (lightgray2 "brightcyan")
          (pink "magenta")
+         (purple "brightmagenta")
          (darkgray "brightblue")
          (white "brightwhite"))
     (custom-theme-set-faces
@@ -15,15 +16,22 @@
      ;;;;;;;;;;;;;;;;;;;;;;;
                                           ; Direct specification.
      ;;;;;;;;;;;;;;;;;;;;;;;
-     `(default ((t (:foreground ,black ))))
+     ;; `(default ((t (:foreground ,black ))))
      `(isearch ((t (:background ,white :foreground ,pink :inverse-video t))))
      `(lazy-highlight ((t (:foreground ,lightgray :background ,black :inverse-video t))))
      `(hl-line ((t :background ,lightgray)))
      `(highlight ((t (:background ,lightgray))))
+
      `(font-lock-keyword-face ((t (:foreground ,cyan))))
-     `(font-lock-builtin-face ((t (:foreground ,blue))))
-     `(font-lock-constant-face ((t (:foreground ,pink))))
-     `(font-lock-string-face ((t (:foreground ,black))))
+     `(font-lock-builtin-face ((t (:foreground ,orange))))
+     `(font-lock-constant-face ((t (:inherit default))))
+     `(font-lock-string-face ((t (:foreground ,cyan))))
+     `(font-lock-function-name-face ((t (:foreground ,pink))))
+     `(font-lock-comment-face ((t (:foreground ,gray))))
+     `(font-lock-keyword-face ((t (:foreground ,purple))))
+     `(font-lock-variable-name-face ((t (:underline t))))
+     `(font-lock-type-face ((t (:inherit default))))
+ 
      `(error ((t (:inverse-video t :foreground ,red))))
      `(region ((t (:background ,black :foreground ,white))))
 
@@ -32,7 +40,6 @@
      `(mode-line-inactive ((t (:foreground ,gray :background ,white :inverse-video nil))))
      `(vertical-border ((t (:foreground ,lightgray :background ,lightgray))))
      `(match ((t (:underline t :weight bold :foreground ,pink))))
-     `(font-lock-comment-face ((t (:foreground ,gray))))
      '(header-line ((t (:weight bold :inverse-video nil :background nil))))
      '(show-paren-match ((t (:background nil :foreground nil :inverse-video t))))
      '(helm-source-header ((t (:inherit default :weight bold :underline t))))
@@ -56,19 +63,21 @@
                                           ; Pure inherited aliases.
      ;;;;;;;;;;;;;;;;;;;;;;;;;
      '(secondary-selection ((t (:inherit highlight))))
-     '(web-mode-html-tag-face ((t (:inherit font-lock-variable-name-face))))
-      '(web-mode-html-attr-name-face ((t (:inherit font-lock-keyword-face))))
-      '(web-mode-param-name-face ((t (:inherit font-lock-variable-name-face))))
-      '(web-mode-doctype-face ((t (:inherit web-mode-comment-face))))
-      '(web-mode-symbol-face ((t (:inherit web-mode-comment-face))))
-      '(web-mode-current-element-highlight-face ((t (:inherit highlight))))
+     '(web-mode-html-tag-face ((t (:inherit default :weight bold))))
+     '(web-mode-css-property-name-face ((t (:inherit default))))
+     '(web-mode-css-selector-face ((t (:inherit default))))
+     '(web-mode-html-attr-name-face ((t (:inherit default))))
+     '(web-mode-param-name-face ((t (:inherit font-lock-variable-name-face))))
+     '(web-mode-doctype-face ((t (:inherit web-mode-comment-face))))
+     '(web-mode-symbol-face ((t (:inherit web-mode-comment-face))))
+     '(web-mode-current-element-highlight-face ((t (:inherit highlight))))
 
-      '(ac-completion-face ((t (:inherit font-lock-comment-face))))
-      '(ac-emacs-eclim-candidate-face ((t (:inherit ac-candidate-face))))
-      '(ac-emacs-eclim-selection-face ((t (:inherit ac-selection-face))))
-      '(popup-tip-face ((t (:inherit secondary-selection))))
+     '(ac-completion-face ((t (:inherit font-lock-comment-face))))
+     '(ac-emacs-eclim-candidate-face ((t (:inherit ac-candidate-face))))
+     '(ac-emacs-eclim-selection-face ((t (:inherit ac-selection-face))))
+     '(popup-tip-face ((t (:inherit secondary-selection))))
 
-      '(helm-candidate-number ((t (:inherit mode-line))))
-      '(helm-selection ((t (:inherit highlight))))))
+     '(helm-candidate-number ((t (:inherit mode-line))))
+     '(helm-selection ((t (:inherit highlight))))))
 
 (provide 'my-theme)
