086175c23e717651da6e206cd6bd2cd42192a9a0
Michael Lin <mtlin@google.com>
2014-07-14 17:01:27 -0400

    Get working directory properly.

--- serve.go
+++ serve.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"fmt"; "log"; "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("Serving files in the current directory on port 8080")
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	wd, _ := os.Getwd()
+	http.Handle("/", http.FileServer(http.Dir(wd)))
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
